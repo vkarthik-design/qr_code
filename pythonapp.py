f865c04824ac247ed456d7a5ae602b60725b154e
V Karthik <v34604644@gmail.com>
2026-05-06 13:46:04 +0530

    Update pythonapp.py

--- pythonapp.py
+++ pythonapp.py
@@ -1,24 +1,15 @@
-
+import streamlit as st
 import qrcode
-from matplotlib import pyplot as plt
-from urllib.parse import quote
-
-upi_id = "8660972945@ptsbi"
-name = "RavishankarAnkir"
-amount = "10000"
-
-# Encode name properly
-name = quote(name)
-
-# Create UPI payment link
-upi_link = f"upi://pay?pa={upi_id}&pn={name}&am={amount}&cu=INR"
-
-print(upi_link)
-
-# Generate QR code
-img = qrcode.make(upi_link)
-
-# Display QR
-plt.imshow(img)
-plt.axis("off")
-plt.show()
+from PIL import Image
+st.title("Qr code generator")
+data=st.text_input("enter url")
+if st.button("Generate QR"):
+  if data:
+    qr = qrcode.make(data)
+    qr.save("qr.png")
+    img = Image.open("qr.png")
+    st.image(img, caption="Generated QR Code")
+    with open("qr.png","rb") as f:
+      st.download_button("Download QR",f,file_name="qr.png")
+  else:
+    st.warning("Please enter some text")
