3d877b479b23e9d315b2b7040de7f1de6641b6cd
V Karthik <v34604644@gmail.com>
2026-05-06 13:27:39 +0530

    Update pythonapp.py

--- pythonapp.py
+++ pythonapp.py
@@ -1,12 +1,54 @@
 import qrcode
+from IPython.display import display
+import ipywidgets as widgets
+from IPython.display import clear_output
 
-upi_id = "8951136490-2@ibl"
-bank_name = "Indian Post Payments Bank User"
-upi_link = f"upi://pay?pa={upi_id}&pn={bank_name}"
 
-qr = qrcode.make(upi_link)
+text_box = widgets.Text(
+    value='https://www.google.com',
+    description='Link:',
+    layout=widgets.Layout(width='500px')
+)
 
-qr.save("payment_qr.png")
 
-display(qr)
-display(qrcode.image)
+button = widgets.Button(
+    description='Generate QR',
+    button_style='success'
+)
+
+
+output = widgets.Output()
+
+def generate_qr(b):
+
+    with output:
+
+        clear_output()
+
+        
+        data = text_box.value
+
+        qr = qrcode.QRCode(
+            version=4,
+            box_size=10,
+            border=5
+        )
+
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(
+            fill_color="black",
+            back_color="white"
+        )
+
+        img.save("website_qr.png")
+
+        display(img)
+
+
+button.on_click(generate_qr)
+
+display(text_box)
+display(button)
+display(output)
